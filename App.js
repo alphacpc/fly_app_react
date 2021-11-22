9205af554391060d96b59ddbe3ee34332d64749e
alphacpc <alphacpc@gmail.com>
2021-11-22 19:37:39 +0000

    Update project

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+
+const Stack = createStackNavigator();
+
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Add App!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <Stack.Screen name="Home" component={HomeScreen}/> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+})
