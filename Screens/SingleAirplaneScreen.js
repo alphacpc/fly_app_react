812da01e4991917d7780dd3bf14301b9706d8ef4
alphacpc <alphacpc@gmail.com>
2021-12-03 12:13:41 +0000

    update SingleAirplaneScreen

--- Screens/SingleAirplaneScreen.js
+++ Screens/SingleAirplaneScreen.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, ScrollView, Image, Pressable, ActivityIndicator, ActionSheetIOS } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, Dimensions, Image, Pressable, ActivityIndicator, ActionSheetIOS } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+
+import Avatar from '../assets/images/FlagSN.png';
+
 
 import {useFonts,
     Inter_100Thin,
@@ -12,6 +15,8 @@ import {useFonts,
     Inter_700Bold,
     Inter_800ExtraBold,
     Inter_900Black,} from '@expo-google-fonts/inter';
+
+const widthScreen = Dimensions.get('screen').width;
 
 const SingleAirplaneScreen = ({navigation,route}) => {
     
@@ -33,18 +38,21 @@ const SingleAirplaneScreen = ({navigation,route}) => {
             <ScrollView showsVerticalScrollIndicator={false}>
                 {/* Header SingleAirplaneScreen */}
                 <View style={styles.ViewHeader}>
-                    <Ionicons name="md-chevron-back-outline" size={24} color="black" />
-                    <Text>Detail du vol</Text>
+                    <View style={styles.ViewHeaderContentLeft} >
+                        <Ionicons name="md-chevron-back-outline" size={24} color="black" />
+                        <Text style={styles.TextTitleScreen}>Détail du vol</Text>
+                    </View>
+                    <Image source={Avatar} style={styles.Avatar}/>
                 </View>
                 {/* Body SingleAirplaneScreen */}
-                <View>
+                <View style={styles.ViewBody}>
                     <Text>{item.airline.name}</Text>
                 </View>
 
             </ScrollView>
         </SafeAreaView>
     ): (
-        <SafeAreaView>
+        <SafeAreaView style={styles.SingleAirplaneContainer}>
             <View style={{paddingTop:40}}>
                 <Text>En chargement...</Text>
                 <ActivityIndicator color="#f05"/>
@@ -56,6 +64,43 @@ const SingleAirplaneScreen = ({navigation,route}) => {
 export default SingleAirplaneScreen;
 
 const styles = StyleSheet.create({
-    SingleAirplaneContainer:{},
-    SingleAirplaneContainer:{},
+    SingleAirplaneContainer:{
+        backgroundColor:'#97BFB4',
+        paddingTop: 24,
+    },
+    ViewHeader:{
+        backgroundColor:'#97BFB4',
+        paddingHorizontal: 10,
+        paddingTop: 10,
+        paddingBottom: 20,
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent: 'space-between'
+    },
+    ViewHeaderContentLeft:{
+        flexDirection:'row',
+        alignItems:'center',
+    },
+    TextTitleScreen:{
+        marginLeft: 10,
+        fontFamily: 'Inter_600SemiBold',
+        letterSpacing: 0.4,
+        fontSize: 18
+    },
+    Avatar:{
+        width: 40,
+        height: 40,
+        borderRadius:50,
+        elevation: 10
+    },
+    // View Body
+    ViewBody:{
+        paddingHorizontal: 20,
+        paddingTop:20,
+        paddingBottom: 40,
+        height: widthScreen * 2,
+        borderTopLeftRadius: 50,
+        borderTopRightRadius: 50,
+        backgroundColor: '#ffffff',
+    }
 })
